32d8651586477f52e12b11d93a713101f9009b46
administrator <3120372227@qq.com>
2024-04-08 10:31:22 +0800

    2024.4.8

---
+++ secondhand-deal-vue/src/components/common/Common.vue
@@ -0,0 +1,17 @@
+<template>
+  <div style="width: 100%; height: 60px; background-color: #68cecd">
+
+  </div>
+
+
+</template>
+
+<script>
+export default {
+  name: "Common"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ secondhand-deal-vue/src/components/home/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div style="width: 100%; height: 100%; background-color: #42b983">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home"
+}
+</script>
+
+<style scoped>
+
+</style>
